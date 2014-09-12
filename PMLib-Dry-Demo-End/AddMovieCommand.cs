b563e203e07427c80d75f101829f049af75ba27a
Adam Killander <adam.killander@adaptiv.se>
2014-09-13 01:06:36 +0200

    Nytt namn och så verkar jag behöva lägga till filerna igen!

---
+++ PMLib-Dry-Demo-End/AddMovieCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace PMLibDryDemoEnd
+{
+    public class AddMovieCommand : Command
+    {
+        public AddMovieCommand(string title, string director, int minutes, string rating)
+            : base(title, director, minutes.ToString(), rating)
+        {
+        }
+
+        protected override byte[] CommandChar
+        {
+            get { return new byte[] { 0x02 }; }
+        }
+    }
+}
---
+++ PMLib-Dry-Demo-End/LoginCommand.cs
@@ -0,0 +1,17 @@
+﻿
+namespace PMLibDryDemoEnd
+{
+    public class LoginCommand : Command
+    {
+        public LoginCommand(string userName, string password)
+            : base(userName, password)
+        {
+        }
+
+        protected override byte[] CommandChar
+        {
+            get { return new byte[] { 0x01 }; }
+        }
+    }
+
+}
